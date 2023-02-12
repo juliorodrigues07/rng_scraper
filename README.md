94f24f7eb1941032bed05aae6f893c330c0cfaf3
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-02-11 22:36:59 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# rng_scrapper
- RNG Scrapper built utilizing Selenium library.
+ [![Python 3.10.6](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)](https://www.python.org/downloads/release/python-3106/)
+
+# RNG Scrapper 
+
+Cryptographically secure random number scrapper built utilizing Selenium library.
+
+# Requirements
+
+- [Selenium](https://www.selenium.dev/pt-br/documentation/) library
+
+    pip install selenium
+      
+- [Random User Agents](https://pypi.org/project/random-user-agent/) library
+ 
+    pip install random_user_agent
+       
+- [PyCryptodome](https://pycryptodome.readthedocs.io/en/latest/src/introduction.html) library
+ 
+    pip install pycryptodomex
+       
+- To install all dependencies:
+
+    ./install_dependencies.sh
+       
+
+# Execution
+
+    python3 scrapper.py
+
+
+# Notes
+
+- The Selenium library requires the setup of environment variables, as also the download of a driver accordingly to version and browser utilized.
+- That driver must be put in the directory setted by the environment variable before running the script, as well as changing it's value if necessary.
---
+++ src/scrapper.py
@@ -0,0 +1,116 @@
+from random_user_agent.params import OperatingSystem
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName
+from selenium.webdriver.common.by import By
+from Cryptodome.Random import random
+from selenium import webdriver
+from time import sleep
+import os
+
+
+# Total numbers to generate, their interval and the generating webpage
+total = 10000000
+min = pow(10, 11)
+max = pow(10, 12) - 1
+rng_url = 'https://www.gigacalculator.com/calculators/random-number-generator.php'
+
+# Generation of the user-agent strings (for distinct OS and browsers)
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.EDGE.value, SoftwareName.OPERA.value, SoftwareName.SAFARI.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.ANDROID.value, OperatingSystem.IOS.value, OperatingSystem.MACOS.value]   
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=500)
+
+my_path = r'/home/julio/Downloads/Software'
+if not os.path.isdir(my_path + r'/SeleniumDrivers'):
+    os.mkdir(my_path + r'/SeleniumDrivers')
+
+# Added new environment variable for the browsers' drivers
+drivers_dir = my_path + r'/SeleniumDrivers'
+os.environ['PATH'] += drivers_dir
+
+
+def initialize_browser():
+
+    fox_profile = webdriver.FirefoxOptions()
+    fox_profile.add_argument("--headless")
+    fox_profile.add_argument("--width=800")
+    fox_profile.add_argument("--height=600")
+
+    # Requests as a distinct user-agent on each request to avoid IP block
+    fox_profile.set_preference('general.useragent.override', user_agent_rotator.get_random_user_agent)
+    my_browser = webdriver.Firefox()
+
+    # Requests the page and awaits it's loading
+    my_browser.get(rng_url)
+    my_browser.implicitly_wait(5)
+
+    return my_browser
+
+
+def auto_generation(my_browser):
+
+    # Selects the form fields to fullfill
+    min_field = my_browser.find_element(By.ID, 'numbersfrom')
+    max_field = my_browser.find_element(By.ID, 'numbersto')
+    qtt_field = my_browser.find_element(By.ID, 'number')
+
+    # Selects the information submit button (request)
+    submit_button = my_browser.find_element(By.CSS_SELECTOR, 'button[class="btn btn-primary btn-lg"]')
+
+    # Clears pre-filled data in fields
+    min_field.clear()
+    max_field.clear()
+    qtt_field.clear()
+
+    # Inserting information in the fields
+    min_field.send_keys(min)
+    max_field.send_keys(max)
+
+    # Amount of numbers to be generated is defined randomly on each request (website limit -> 1000)
+    n = random.randint(1, 1000)
+    qtt_field.send_keys(n)
+
+    # Awaits for the complete fullfill of the fields for then execute the request and receive it's response
+    my_browser.implicitly_wait(2)
+    submit_button.click()
+
+    # Obtains the text containing the generated numbers in the request's response (tag HTML <th>)
+    calc_row = my_browser.find_element(By.TAG_NAME, 'th').text
+
+    # Converts every number in the list to integer (str -> int)
+    str_numbers = calc_row.split(', ')
+    numbers = list(map(int, str_numbers))
+    return numbers
+
+
+def randomness_evaluation():
+    
+    print('Initializing...\n')
+    all_numbers = list()
+    my_browser = initialize_browser()
+
+    # Until enough number are generated
+    while len(all_numbers) < total:
+
+        # Fill in the webpage fields, gets the generated numbers and add them to the list
+        numbers = auto_generation(my_browser)
+        all_numbers += numbers
+        
+        del(numbers)
+        progress = (len(all_numbers) / total) * 100.0
+        print(f'{round(progress, 2)}% concluded...')
+
+        # Waiting time defined randomly between each request to avoid IP block
+        timing = random.randint(2, 4)
+        sleep(timing)
+
+    my_browser.close()
+
+    # Result
+    print('\nFinalized!\n')
+    size = len(all_numbers)
+    uniques = len(set(all_numbers))
+    print(f'{size - uniques} repeats in {size} generated numbers!')
+
+
+if __name__ == '__main__':
+    randomness_evaluation()
